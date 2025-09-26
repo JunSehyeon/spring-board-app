4efae733e0d8ebc47bbe92ca90944cb0ad3b0280
JunSehyeon <shjun1210@icloud.com>
2025-09-26 11:57:56 +0900

    commit 0.0.2

--- src/main/java/kr/co/sboard/controller/UserController.java
+++ src/main/java/kr/co/sboard/controller/UserController.java
@@ -1,17 +1,25 @@
 package kr.co.sboard.controller;
 
+import kr.co.sboard.config.AppInfo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+    private final AppInfo appInfo;
     @GetMapping("/user/info")
     public String info(){
         return null;
     }
     @GetMapping("/user/login")
-    public String login(){
-        return null;
+    public String login(Model model){
+
+        model.addAttribute(appInfo);
+        return "user/login";
+
     }
     @GetMapping("/user/register")
     public String register(){
